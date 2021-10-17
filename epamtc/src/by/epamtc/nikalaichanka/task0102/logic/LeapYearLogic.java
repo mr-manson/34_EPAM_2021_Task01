5b1d907ca43a877c93e9c7edc28eeb2806166d0c
mr-manson <60350677+mr-manson@users.noreply.github.com>
2021-10-17 18:34:49 +0300

    Add files via upload

--- epamtc/src/by/epamtc/nikalaichanka/task0102/logic/LeapYearLogic.java
+++ epamtc/src/by/epamtc/nikalaichanka/task0102/logic/LeapYearLogic.java
@@ -3,10 +3,14 @@ package by.epamtc.nikalaichanka.task0102.logic;
 import by.epamtc.nikalaichanka.task0102.mounth.Mounth;
 
 public class LeapYearLogic {
+	
+	public static final int FOUR_HUND_DIV = 400;
+	public static final int FOUR_DIV = 4;
+	public static final int HUND_DIV = 100;
 
-	public static boolean leapYear(int year) {
+	public static boolean isLeapYear(int year) {
 		boolean leap = false;
-		if ((year % 400 == 0 || year % 4 == 0) && (year % 100 != 0)) {
+		if ((year % FOUR_HUND_DIV == 0 || year % FOUR_DIV == 0) && (year % HUND_DIV != 0)) {
 			leap = true;
 		}
 		return leap;
@@ -16,10 +20,9 @@ public class LeapYearLogic {
 		int c = 0;
 		for (Mounth m : Mounth.values()) {
 			if (mounth - 1 == m.ordinal()) {
-				if (mounth == 2 && LeapYearLogic.leapYear(year)) {
-					c = m.getQuantity() + 1;
-				} else {
-					c = m.getQuantity();
+				c = m.getQuantity();
+				if (mounth == 2 && LeapYearLogic.isLeapYear(year)) {
+					c++;
 				}
 			}
 		}
